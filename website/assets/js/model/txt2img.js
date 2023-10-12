f5256a3251062447828fcb2c33ec0db0a2a5e3c7
BONAFIDE BOSS <amaanalmir@gmail.com>
2023-10-12 15:24:50 +0300

    update

---
+++ website/assets/js/model/txt2img.js
@@ -0,0 +1,113 @@
+const models = ['text2img', 'stable-diffusion']
+
+$('#generate-image').on('submit', async function (e) {
+  e.preventDefault()
+  $(this).find('button').attr('disabled', true)
+  show_new_image_loader()
+
+  var query = Object.fromEntries(new FormData(this))
+  if (query.prompt == "") {
+    return
+  }
+
+  if (!(models.includes(query.model))) {
+    return
+  }
+
+  var img_width = "512"
+  var img_height = "512"
+
+  const options = {
+    "text": query.prompt,
+    "grid_size": "1",
+    "width": img_width,
+    "height": img_height,
+  }
+
+  var image = await generate_image(query.model, options)
+  if (image) {
+    display_new_image(image)
+  } else {
+    display_fail_message()
+  }
+
+  $(this).find('button').attr('disabled', false)
+})
+
+async function generate_image(model, options) {
+  image = ""
+  try {
+    deepai.setApiKey('quickstart-QUdJIGlzIGNvbWluZy4uLi4K');
+    var result = await deepai.callStandardApi(model, options);
+    if (result.output_url) {
+      image = await upload_to_cloud(result.output_url)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+  return image
+}
+
+function show_new_image_loader() {
+  var loader = `
+    <div class="col generating-image placeholder-wave">
+      <div class="card text-bg-dark h-100 border-0">
+        <img src="/assets/img/logo/logo-full.png" class="card-img opacity-0 placeholder h-100">
+        <div class="card-img-overlay text-center d-flex flex-column justify-content-center">
+          <h5 class="card-title">Generating image...</h5>
+          <p class="card-text text-secondary">Please hold on! This may take a while.</p>
+        </div>
+      </div>
+    </div>
+  `
+  $('#ai-images').prepend(loader)
+  scroll_to_new_image()
+}
+
+function display_new_image(image) {
+  image = `<img src="${image}" width="100%">`
+  $('.generating-image').html(image)
+  $('.generating-image').removeClass('generating-image placeholder-wave')
+}
+
+function display_fail_message() {
+  $('.generating-image').removeClass('placeholder-wave')
+  $('.generating-image .placeholder').removeClass('placeholder')
+  $('.generating-image .card-title').html('Failed to generate image')
+  $('.generating-image .card-text').html('Please try again later or contact the developer.')
+
+  setTimeout(() => {
+    $('.generating-image').remove()
+  }, 10000);
+}
+
+function scroll_to_new_image() {
+  document.querySelector('.generating-image').scrollIntoView({
+    behavior: 'auto',
+    block: 'center',
+    inline: 'center'
+  });
+}
+
+async function upload_to_cloud(image) {
+  const api_key = 'd5189c1fb088a3558b2c8ca0fae2e392'
+  const url = 'https://api.imgbb.com/1/upload'
+
+  const form = new FormData()
+  form.append('key', api_key)
+  form.append('image', image)
+
+  await $.ajax({
+    url: url,
+    data: form,
+    cache: false,
+    contentType: false,
+    processData: false,
+    method: 'POST',
+    success: function (response) {
+      image = response.data.url
+    }
+  })
+
+  return image
+}
